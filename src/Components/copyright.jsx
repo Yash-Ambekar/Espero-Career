a0b98b2a7e7f34506cf8cf948375a7c1e42f83ba
Pankaj Jangir <pankajjangir20@cse.iiitp.ac.in>
2022-02-28 14:00:22 +0530

    Copyright

---
+++ src/Components/copyright.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../CSS/copyright.css";
+
+const Copyright = () => {
+  return (
+    <div className="CopyrightContainer">
+      <span className="m-auto">Copyright © Espero Career Institure</span>
+      <span className="m-auto">Contact us | Rules and Regulations</span>
+    </div>
+  );
+};
+
+export default Copyright;
