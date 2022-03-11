022268beb1ee382f711733b5a1e629a3e40a992e
Pankaj Jangir <pankajjangir20@cse.iiitp.ac.in>
2022-03-11 15:40:33 +0530

    Slideshow Fixed

--- src/Components/Slideshow.jsx
+++ src/Components/Slideshow.jsx
@@ -1,53 +1,60 @@
 import { getDownloadURL, listAll, ref } from "firebase/storage";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Fade } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 import "../CSS/slideshow.css";
 import storage from "../firebase";
 
-const fadeImages = [""];
-
-const handleImages = async (data) => {
-  const folderRef = ref(storage, "SlideShow/");
-  // console.log(folderRef);
-  const result = await listAll(folderRef);
-
-  result.items.forEach((itemRef) => {
-    console.log(itemRef);
-    // setImages(itemRef._location.path_).then((res) => console.log(res));
-    setImages(itemRef._location.path_).then((res) => {
-      fadeImages.push(res);
-    });
-  });
-  // fadeImages.push(itemRef._location.path_);
-
-  console.log(fadeImages);
-};
-
-const setImages = async (imageURL) => {
-  return await getDownloadURL(ref(storage, imageURL));
-};
-
 const Slideshow = () => {
+  const [images, setImagesURL] = useState([]);
+
+  const handleImages = async () => {
+    const fadeImages = [];
+    const folderRef = ref(storage, "SlideShow/");
+    const result = await listAll(folderRef);
+
+    if (result) {
+      for (let itemRef of result.items) {
+        const imageURL = await getDownloadURL(
+          ref(storage, itemRef._location.path_)
+        );
+        console.log(imageURL);
+        fadeImages.push(imageURL);
+      }
+      setImagesURL(fadeImages);
+    }
+
+    console.log(fadeImages, "Before 30");
+  };
+
+  useEffect(() => {
+    console.log(images, "Hemlo");
+  }, [images]);
+
   useEffect(() => {
     handleImages();
   }, []);
 
   return (
     <div className="slide-container">
-      <Fade indicators={true} cssClass="slideShowCustom">
-        {fadeImages.map((fadeImage, index) => (
-          
-          <div className="each-fade" key={index}>
-            <div className="flex justify-center image-container">
-              <img src={fadeImage.url} alt= "" className="h-[150px] sm:h-[300px] md:h-[350px] w-full images" />
+      {images.length > 0 ? (
+        <Fade indicators={true}>
+          {images.map((fadeImage, index) => (
+            <div key={index}>
+              <div className="flex justify-center image-container">
+                <img
+                  src={fadeImage}
+                  alt={fadeImage}
+                  className="h-[150px] sm:h-[300px] md:h-[350px] w-full images"
+                />
+              </div>
             </div>
-          </div>
-          
-        ))}
-      </Fade>
+          ))}
+        </Fade>
+      ) : (
+        "null"
+      )}
     </div>
-    
   );
 };
 
